a7f92c51460ac39cfadd8d6478d9fe7047953fb4
Pantagon <pichayoothunchainao@gmail.com>
2023-07-19 20:35:44 +0700

    add bottom navbar by daisyui

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,14 @@
 'use client'
 import Login from "./LoginPage"
 import Chart from "../Component/DonughnutChart"
+import Dashboard from "./DashboardPage"
 
 export default function Home(){
   return(
     <div>
       {/* <Login /> */}
-      <Chart/>
+      {/* <Chart/> */}
+      <Dashboard />
     </div>
   )
 }
