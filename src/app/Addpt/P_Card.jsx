cd55c507485d2b0cdbfda3e6c5e7b3969fcb9d52
640610645 <nontapan_chanadee@cmu.ac.th>
2023-08-10 19:11:20 +0700

    เพิ่ม modal

--- src/app/Addpt/P_Card.jsx
+++ src/app/Addpt/P_Card.jsx
@@ -1,6 +1,25 @@
-import { Card, Text } from "@mantine/core";
+import { Card } from "@mantine/core";
+// import { modals } from "@mantine/modals";
+import * as React from "react";
+import Button from "@mui/material/Button";
+import Dialog from "@mui/material/Dialog";
+import DialogActions from "@mui/material/DialogActions";
+import DialogContent from "@mui/material/DialogContent";
+import DialogContentText from "@mui/material/DialogContentText";
+import DialogTitle from "@mui/material/DialogTitle";
+import CheckIcon from "@mui/icons-material/Check";
 
 export default function P_Card() {
+  const [open, setOpen] = React.useState(false);
+
+  const handleClickOpen = () => {
+    setOpen(true);
+  };
+
+  const handleClose = () => {
+    setOpen(false);
+  };
+
   return (
     <div>
       <Card
@@ -29,10 +48,7 @@ export default function P_Card() {
             <span className="IDNo">XXXXXXXXXX</span>
           </div>
         </div>
-        <button
-          className="flex w-[45px] h-[40px] bg-transparent mr-[5px] mt-[15px] pt-[3px] border-[none]"
-          type="button"
-        >
+        {/* <Button onClick={handleClickOpen}>
           <svg
             xmlns="http://www.w3.org/2000/svg"
             width="30"
@@ -49,7 +65,106 @@ export default function P_Card() {
               fill="#292D32"
             />
           </svg>
-        </button>
+        </Button>
+        <Dialog
+          open={open}
+          onClose={handleClose}
+          aria-labelledby="alert-dialog-title"
+          aria-describedby="alert-dialog-description"
+        >
+          <DialogTitle id="alert-dialog-title">{"เพิ่มคนไข้"}</DialogTitle>
+          <DialogContent>
+            <DialogContentText id="alert-dialog-description">
+              Name: นาย ณัฐพล สายทอง
+            </DialogContentText>
+            <DialogContentText id="alert-dialog-description">
+              ID: 1XXXXXXXXXXXX
+            </DialogContentText>
+          </DialogContent>
+          <DialogActions>
+            <Button onClick={handleClose} color="error">
+              ยกเลิก
+            </Button>
+            <Button onClick={handleClose} color="success" autoFocus>
+              ตกลง
+            </Button>
+          </DialogActions>
+        </Dialog> */}
+        <div className="pt-[10px]">
+          <CheckIcon sx={{ fontSize: "50px" }} />
+        </div>
+      </Card>
+      <Card
+        // shadow="lg"
+        // padding="xl"
+        // component="a"
+        // href="https://www.youtube.com/watch?v=dQw4w9WgXcQ"
+        className="w-4/5 flex flex-row justify-between bg-[#d0ebfd] font-semibold mb-[5px] mx-[10%] pl-[30px] rounded-[10px]"
+      >
+        {/* <Text weight={500} size="lg" mt="md">
+          You&apos;ve won a million dollars in cash!
+        </Text>
+
+        <Text mt="xs" color="dimmed" size="sm">
+          Please click anywhere on this card to claim your reward, this is not a
+          fraud, trust us
+        </Text> */}
+
+        <div className="PText">
+          <div className="mt-[10px]">
+            <span className="NameTitle">นาย </span>
+            <span className="PNameText">ณัฐพล สายทอง</span>
+          </div>
+          <div className="mt-[1px] mb-[10px]">
+            <span className="IDText">ID </span>
+            <span className="IDNo">XXXXXXXXXX</span>
+          </div>
+        </div>
+        <Button onClick={handleClickOpen}>
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            width="30"
+            height="30"
+            viewBox="0 0 46 46"
+            fill="none"
+          >
+            <path
+              d="M43.4444 25.5556H2.55556C1.15852 25.5556 0 24.397 0 23C0 21.603 1.15852 20.4444 2.55556 20.4444H43.4444C44.8415 20.4444 46 21.603 46 23C46 24.397 44.8415 25.5556 43.4444 25.5556Z"
+              fill="#292D32"
+            />
+            <path
+              d="M23 46C21.603 46 20.4445 44.8415 20.4445 43.4444V2.55554C20.4445 1.1585 21.603 -1.52588e-05 23 -1.52588e-05C24.3971 -1.52588e-05 25.5556 1.1585 25.5556 2.55554V43.4444C25.5556 44.8415 24.3971 46 23 46Z"
+              fill="#292D32"
+            />
+          </svg>
+        </Button>
+        <Dialog
+          open={open}
+          onClose={handleClose}
+          aria-labelledby="alert-dialog-title"
+          aria-describedby="alert-dialog-description"
+        >
+          <DialogTitle id="alert-dialog-title">{"เพิ่มคนไข้"}</DialogTitle>
+          <DialogContent>
+            <DialogContentText id="alert-dialog-description">
+              Name: นาย ณัฐพล สายทอง
+            </DialogContentText>
+            <DialogContentText id="alert-dialog-description">
+              ID: 1XXXXXXXXXXXX
+            </DialogContentText>
+          </DialogContent>
+          <DialogActions>
+            <Button onClick={handleClose} color="error">
+              ยกเลิก
+            </Button>
+            <Button onClick={handleClose} color="success" autoFocus>
+              ตกลง
+            </Button>
+          </DialogActions>
+        </Dialog>
+        {/* <div className="pt-[10px]">
+          <CheckIcon sx={{ fontSize: "50px" }} />
+        </div> */}
       </Card>
     </div>
   );
