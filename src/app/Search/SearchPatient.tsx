419cfc1c6a82e84e643a07c1923a847a2069b836
Pantagon <pichayoothunchainao@gmail.com>
2023-09-05 18:11:20 +0700

    Add modal

--- src/app/Search/SearchPatient.tsx
+++ src/app/Search/SearchPatient.tsx
@@ -1,67 +1,107 @@
-import React from "react";
+import React, { useState } from "react";
 import { Card } from "@mantine/core";
+import { useCounter } from "@mantine/hooks";
+import { Modal, Button, Group, Text, Badge } from "@mantine/core";
+import AddIcon from "@mui/icons-material/Add";
 
 interface SearchResult {
-  f_name: string;
-  l_name: string;
-  identification_id: string;
-  status: string;
+    f_name: string;
+    l_name: string;
+    identification_id: string;
+    status: string;
 }
 
 interface SearchPatientProps {
-  apiData: SearchResult[];
+    apiData: SearchResult[];
 }
 
 const getStatusColor = (status: string) => {
-  switch (status) {
-    case "STATUS_1":
-      return "#65EB89"; 
-    case "STATUS_2":
-      return "#949DEB"; 
-    case "STATUS_3":
-      return "#EBD071"; 
-    case "STATUS_4":
-      return "#EB6569"; 
-    default:
-      return "#000000"; // Default color for unknown statuses
-  }
+    switch (status) {
+        case "STATUS_1":
+            return "#65EB89";
+        case "STATUS_2":
+            return "#949DEB";
+        case "STATUS_3":
+            return "#EBD071";
+        case "STATUS_4":
+            return "#EB6569";
+        default:
+            return "#000000"; // Default color for unknown statuses
+    }
 };
 
 const SearchPatient: React.FC<SearchPatientProps> = ({ apiData }) => {
-  return (
-    <div>
-      {apiData.map((item, index) => (
-        <Card
-          className="w-4/5 flex flex-row justify-between bg-[#BFDBFE] font-semibold mb-[15px] mx-[10%] pl-[30px] pr-[20px] rounded-[10px] text-[#2563EB]"
-          key={index}
-        >
-          <div className="PText">
-            <div className="mt-[8px]">
-              {/* Circle with background color based on status */}
-              <div
-                style={{
-                  backgroundColor: getStatusColor(item.status),
-                  width: "20px",
-                  height: "20px",
-                  borderRadius: "50%",
-                  display: "inline-block",
-                  marginRight: "10px",
-                }}
-              />
-              {item.f_name} {item.l_name}
-              <div />
-              {item.identification_id}
-              {/* <div />
-              {item.status} */}
-            </div>
-          </div>
-          <div className="py-[10px]">
-            {/* Add any additional content or buttons here */}
-          </div>
-        </Card>
-      ))}
-    </div>
-  );
+    const [opened, setOpened] = useState(false);
+    const [selectedCard, setSelectedCard] = useState<SearchResult | null>(null);
+
+    const handleCardClick = (item: SearchResult) => {
+        setSelectedCard(item);
+        setOpened(true);
+    };
+
+    const closeModal = () => {
+        setSelectedCard(null);
+        setOpened(false);
+    };
+
+    return (
+        <div>
+            {apiData.map((item, index) => (
+                <Card
+                    className="w-4/5 flex flex-row justify-between bg-[#BFDBFE] font-semibold mb-[15px] mx-[10%] pl-[30px] pr-[0px] rounded-[10px] text-[#2563EB]"
+                    key={index}
+                    // onClick={() => console.log("Go to patient data page")} onclick ไปหน้า patientpage
+                >
+                    <div className="PText">
+                        <div className="mt-[8px]">
+                            <div
+                                style={{
+                                    backgroundColor: getStatusColor(item.status),
+                                    width: "20px",
+                                    height: "20px",
+                                    borderRadius: "50%",
+                                    display: "inline-block",
+                                    marginRight: "10px",
+                                }}
+                            />
+                            {item.f_name} {item.l_name}
+                            <div />
+                            {item.identification_id}
+                        </div>
+                    </div>
+                    <div className="py-[10px]">
+                        <Button onClick={() => handleCardClick(item)}>
+                            <AddIcon sx={{ fontSize: "45px" }} style={{ color: "#2563EB" }} />
+                        </Button>
+                    </div>
+                </Card>
+            ))}
+            <Modal
+                opened={opened}
+                onClose={closeModal}
+                size="lg"
+                title="ข้อมูลผู้ป่วย"
+            >
+                {selectedCard && (
+                    <div>
+                        <Text>Name: {selectedCard.f_name} {selectedCard.l_name}</Text>
+                        <Text>ID: {selectedCard.identification_id}</Text>
+
+
+                        <Group mt="xl">
+                            <Button variant="outline" color="red" onClick={closeModal}>
+                                Cancel
+                            </Button>
+                            <Button variant="outline" color="green" onClick={() => console.log("Add Patient")}> 
+                            {/* onclick แล้วเพิ่มไปที่Favorite */}
+                                Add Patient
+                            </Button>
+                        </Group>
+                    </div>
+                )}
+            </Modal>
+        </div>
+    );
 };
 
 export default SearchPatient;
