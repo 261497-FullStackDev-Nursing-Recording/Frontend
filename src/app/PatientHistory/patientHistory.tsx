991f70232d7cffbeb5fee26670b6314606b0b959
640610645 <nontapan_chanadee@cmu.ac.th>
2023-09-08 15:41:11 +0700

    ลิงก์ Search ไป patientHistory

--- src/app/PatientHistory/patientHistory.tsx
+++ src/app/PatientHistory/patientHistory.tsx
@@ -2,44 +2,40 @@
 import Navbar from "@/Component/Navbarbottom";
 import "./MyPatientPage.css";
 import Carddata from "../InsideMypatient/Carddata";
-import * as React from 'react';
+import * as React from "react";
 import Spinner from "@/Component/spinner";
-import LocalHospitalRoundedIcon from '@mui/icons-material/LocalHospitalRounded';
-// import { useParams } from "react-router-dom";
-
+import LocalHospitalRoundedIcon from "@mui/icons-material/LocalHospitalRounded";
+import { useParams } from "react-router-dom";
 
 export default function PatientHistory() {
-    const [isPageReady, setIsPageReady] = React.useState(false);
-    // const { identificationId } = useParams<{ identificationId: string }>();
+  const [isPageReady, setIsPageReady] = React.useState(false);
+  const { identification_id } = useParams<{ identification_id: string }>();
+  console.log(identification_id);
 
-    React.useEffect(() => {
-        setTimeout(() => {
-            setIsPageReady(true);
-        }, 300);
-    }, []);
+  React.useEffect(() => {
+    setTimeout(() => {
+      setIsPageReady(true);
+    }, 300);
+  }, []);
 
-    
-
-    return (
-        <div>
-            {isPageReady ? (
-                <div className="Container">
-                    <div>
-                        <LocalHospitalRoundedIcon className="Icon" />
-                    </div>
-                    <div className="Name">Name</div>
-                    <div className="ID">identificationId</div>
-                    <div className="containercard">
-                        
-                    </div>
-                    <div className="card"></div>
-                </div>
-            ) : (
-                <div className="spinner-container">
-                    <Spinner />
-                </div>
-            )}
-            <Navbar />
+  return (
+    <div>
+      {isPageReady ? (
+        <div className="Container">
+          <div>
+            <LocalHospitalRoundedIcon className="Icon" />
+          </div>
+          <div className="Name">Name</div>
+          <div className="ID">identificationId: {identification_id}</div>
+          <div className="containercard"></div>
+          <div className="card"></div>
         </div>
-    );
+      ) : (
+        <div className="spinner-container">
+          <Spinner />
+        </div>
+      )}
+      <Navbar />
+    </div>
+  );
 }
