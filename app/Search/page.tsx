83b1865ee468647fe7ec7969457ef1737b2e9187
640610645 <nontapan_chanadee@cmu.ac.th>
2023-09-21 13:27:49 +0700

    MyPatient+Search: แก้ max width

--- app/Search/page.tsx
+++ app/Search/page.tsx
@@ -67,7 +67,6 @@ export default function Searchpage() {
     // Create a regular expression pattern from the search value
     const searchPattern = new RegExp(valueName.trim(), "i");
     // Filter the data based on the input value for f_name and l_name
-    setApiData([]);
     setApiData(
       data.filter((item: any) => {
         const fullName = `${item.f_name} ${item.l_name}`;
@@ -83,70 +82,67 @@ export default function Searchpage() {
   console.log(data);
 
   return (
-    <div>
-      <div>
-        <div className="flex justify-center mt-[50px]">
-          <PersonSearchRoundedIcon sx={{ fontSize: 40 }} />
-        </div>
-
-        <div className="mb-[40px]">
-          <div className="text-black font-extrabold text-[20px] flex mt-[20px] mb-[5px] pl-[10%]">
-            Citizen ID
-          </div>
-          <div className="flex mx-[10%] relative">
-            <input
-              className="w-full h-[40px] pl-[10px] pr-[10px] bg-[#f5f5f5] text-[15px] rounded-[5px] items-center rounded-tr-none rounded-br-none outline-none"
-              id="identificationid"
-              type="search"
-              placeholder="ID"
-              onChange={(event) => setValueID(event.currentTarget.value)}
-            />
-            <Button
-              variant="contained"
-              style={{ backgroundColor: "#BFDBFE" }}
-              sx={{
-                height: "40px",
-                borderRadius: "5px",
-                borderTopLeftRadius: 0,
-                borderBottomLeftRadius: 0,
-                boxShadow: 0,
-              }}
-              onClick={handleSearchID}
-            >
-              <SearchIcon style={{ color: "#2563EB" }} />
-            </Button>
-          </div>
-          <div className="text-black font-extrabold text-[20px] flex mt-[20px] mb-[5px] pl-[10%]">
-            Name
-          </div>
-          <div className="flex mx-[10%] relative">
-            <input
-              className="w-full h-[40px] pl-[10px] pr-[10px] bg-[#f5f5f5] text-[15px] rounded-[5px] items-center rounded-tr-none rounded-br-none outline-none"
-              id="name"
-              type="search"
-              placeholder="Name"
-              onChange={(event) => setValueName(event.currentTarget.value)}
-            />
-            <Button
-              variant="contained"
-              style={{ backgroundColor: "#BFDBFE" }}
-              sx={{
-                height: "40px",
-                borderRadius: "5px",
-                borderTopLeftRadius: 0,
-                borderBottomLeftRadius: 0,
-                boxShadow: 0,
-              }}
-              onClick={handleSearchName}
-            >
-              <SearchIcon style={{ color: "#2563EB" }} />
-            </Button>
-          </div>
-        </div>
-        <SearchPatient apiData={apiData} />
-        <div style={{ marginBottom: "80px" }} />
-        <Navbar />
+    <div className="max-w-[414px] mx-auto">
+      <div className="flex justify-center mt-[50px]">
+        <PersonSearchRoundedIcon sx={{ fontSize: 40 }} />
       </div>
+      <div className="mb-[40px]">
+        <div className="text-black font-extrabold text-[20px] flex mt-[20px] mb-[5px] pl-[10%]">
+          Citizen ID
+        </div>
+        <div className="flex mx-[10%] relative">
+          <input
+            className="w-full h-[40px] pl-[10px] pr-[10px] bg-[#f5f5f5] text-[15px] rounded-[5px] items-center rounded-tr-none rounded-br-none outline-none"
+            id="identificationid"
+            type="search"
+            placeholder="ID"
+            onChange={(event) => setValueID(event.currentTarget.value)}
+          />
+          <Button
+            variant="contained"
+            style={{ backgroundColor: "#BFDBFE" }}
+            sx={{
+              height: "40px",
+              borderRadius: "5px",
+              borderTopLeftRadius: 0,
+              borderBottomLeftRadius: 0,
+              boxShadow: 0,
+            }}
+            onClick={handleSearchID}
+          >
+            <SearchIcon style={{ color: "#2563EB" }} />
+          </Button>
+        </div>
+        <div className="text-black font-extrabold text-[20px] flex mt-[20px] mb-[5px] pl-[10%]">
+          Name
+        </div>
+        <div className="flex mx-[10%] relative">
+          <input
+            className="w-full h-[40px] pl-[10px] pr-[10px] bg-[#f5f5f5] text-[15px] rounded-[5px] items-center rounded-tr-none rounded-br-none outline-none"
+            id="name"
+            type="search"
+            placeholder="Name"
+            onChange={(event) => setValueName(event.currentTarget.value)}
+          />
+          <Button
+            variant="contained"
+            style={{ backgroundColor: "#BFDBFE" }}
+            sx={{
+              height: "40px",
+              borderRadius: "5px",
+              borderTopLeftRadius: 0,
+              borderBottomLeftRadius: 0,
+              boxShadow: 0,
+            }}
+            onClick={handleSearchName}
+          >
+            <SearchIcon style={{ color: "#2563EB" }} />
+          </Button>
+        </div>
+      </div>
+      <SearchPatient apiData={apiData} />
+      <div style={{ marginBottom: "80px" }} />
+      <Navbar />
     </div>
   );
 }
